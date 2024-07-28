c18b2b7a4a517a599a4440a9f5acc8ae3ba1dd13
gokul-026 <gokulakanna096@gmail.com>
2024-07-28 09:17:34 +0530

    addition.s

--- examples/addition.s
+++ examples/addition.s
@@ -2,8 +2,6 @@
 OPR1: DW 0x6969         ; declare first number
 OPR2: DW 0x0420         ; declare second number
 RESULT: DW 0            ; declare place to store result
-
-; actual entry point of the program
 start:
 MOV AX, word OPR1       ; move first number to AX
 MOV BX, word OPR2       ; move second number to BX
